989df2e9e6e5a0aecbdcc63d274d56919525dced
AdebolaAni <adebolaanimashaun@gmail.com>
2025-03-30 00:01:47 +0100

    jupyter code commit

--- jupyter.sh
+++ jupyter.sh
@@ -3,7 +3,7 @@ sudo su
 yum update -y
 yum install -y httpd
 cd /var/www/html
-wget https://github.com/Ahmednas211/jupiter-zip-file/raw/main/jupiter-main.zip
+wget https://github.com/AdebolaAni/jupiter-zip-file/raw/refs/heads/main/jupiter-main.zip
 unzip jupiter-main.zip
 cp -r jupiter-main/* /var/www/html
 rm -rf jupiter-main jupiter-main.zip
